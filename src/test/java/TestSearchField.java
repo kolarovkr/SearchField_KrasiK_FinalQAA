d505a0046740a475dd66c7681b5751a9abc3a6e9
Krasi K <kolarovkr@gmail.com>
2023-02-26 22:06:39 +0200

    reports text file added

--- src/test/java/TestSearchField.java
+++ src/test/java/TestSearchField.java
@@ -5,8 +5,6 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestSearchField extends TestObject{
-
-
     @DataProvider(name = "getUsers")
     public Object[][] getUsers(){
         return new Object[][]{{"krasiko", "224339K"}, //login with username
@@ -14,7 +12,7 @@ public class TestSearchField extends TestObject{
     }
 
     @Test(dataProvider = "getUsers")
-    public void testSearchFieldIsVisibleAfterLogin(String username, String password){
+    public void testIsSearchBarVisibleAfterLogin(String username, String password){
 
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.logIn(username, password);
